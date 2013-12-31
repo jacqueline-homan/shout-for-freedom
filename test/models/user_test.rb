549e44294e08ebb1a1801a716705354b435ab5c0
Ed Drain <geekprogrammer.ed@gmail.com>
2013-12-31 11:26:19 -0500

    got rid of extraneous test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  context "a user" do
+    setup { User.new }
+    asserts("it's provider is nil") { topic.provider.is_nil? }
+  end
   # test "the truth" do
   #   assert true
   # end
