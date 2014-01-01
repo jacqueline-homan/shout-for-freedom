3a9df6c49e5e8627b0a5e9c485ae800bb4885f89
Ed Drain <geekprogrammer.ed@gmail.com>
2014-01-01 12:40:59 -0500

    commented out logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.logger = Rails.logger
+# OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do 
 	provider :twitter, ENV['TFWS_KEY'], ENV['TFWS_SECRET']
