bb7299b47a46f5ad3034804ff0be39da88624c57
Ed Drain <geekprogrammer.ed@gmail.com>
2014-01-16 12:47:10 -0500

    added logging info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
 
 	def self.from_omniauth(auth)
+		puts "In user.rb, line 4"
 		where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
 	end
 
 	def self.create_from_omniauth(auth)
+		puts "about to create user"
 		create! do |user|
 			user.provider = auth["provider"]
 			user.uid = auth["uid"]
